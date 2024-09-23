e9f086cf97b8b9e2102f5417900debbf84dcef67
Felipe Issa <felipe.issa@zebra.com>
2024-09-23 10:18:29 -0300

    Fixing controller annotation, removing spring plugin version and adding missed interface

--- src/main/java/com/example/jsp_demo/HelloController.java
+++ src/main/java/com/example/jsp_demo/HelloController.java
@@ -2,10 +2,10 @@ package com.example.jsp_demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
--- src/main/java/com/example/jsp_demo/JspDemoApplication.java
+++ src/main/java/com/example/jsp_demo/JspDemoApplication.java
@@ -2,13 +2,20 @@ package com.example.jsp_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {" com.example.jsp_demo"})
-public class JspDemoApplication {
+public class JspDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JspDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JspDemoApplication.class);
 	}
 }
